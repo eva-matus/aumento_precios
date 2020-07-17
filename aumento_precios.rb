5c8469d03ded5b17441e8fb2a3a8887313261498
Eva <eva.matus@hotmail.com>
2020-07-17 11:26:11 -0400

    code identification

--- aumento_precios.rb
+++ aumento_precios.rb
@@ -1,11 +1,11 @@
 def augment(array, factor)
-      new_array = []
-      array.each do |price|
-     new_array.push(price * factor)
-      end
-       new_array
-    end
+    new_array = []
+    array.each do |price|
+    new_array.push(price * factor)
+end
+    new_array
+end
     
-       print augment([10,20,10], 1.2) 
+print augment([10,20,10], 1.2) 
     
     # [12.0, 24.0, 12.0] nos debiera dar este valor
